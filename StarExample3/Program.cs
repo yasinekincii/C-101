81c4bf8113b3203dc01b25d16f27a02590447989
yasinekincii <ekinciyasinn21@gmail.com>
2022-12-16 10:39:02 +0300

    Star Examples

---
+++ StarExample3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarExample3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Kenar Sayısını Giriniz: ");
+            int edge;
+            edge = Convert.ToInt16(Console.ReadLine());
+
+            //üst kenar
+            for(int i=1; i<=edge; i++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+            //sol kenar
+            for (int i=1;i<=edge-2;i++)
+            {
+                Console.Write("*");
+
+                //aradaki boşluklar
+                for(int j=1; j<=edge-2; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                //sağ kenar
+                Console.Write(" *");
+                Console.WriteLine();
+            }
+
+            //alt kenar
+            for(int n=0; n<edge; n++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StarExample4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarExample4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //baklava dilimi
+
+            Console.WriteLine("bir sayı girin");
+            int number;
+            number = Convert.ToInt16(Console.ReadLine());
+
+            for(int i=1; i<=number; i++)
+            {
+               for(int j=number; j>0; j--)
+                {
+                    if (i>=j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            
+            for(int i=number-1; i > 0; i--)
+            {
+                for(int j=number; j>0; j--)
+                {
+                    if (i>=j) 
+                    {
+                        Console.Write("* ");                    
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
