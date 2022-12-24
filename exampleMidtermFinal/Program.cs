c8e17630e48c060a2d152da4f7bd00baec722fdf
yasinekincii <ekinciyasinn21@gmail.com>
2022-12-25 01:26:41 +0300

    For examples

---
+++ exampleMidtermFinal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleMidtermFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Vize notunuzu giriniz= ");
+            int midterm = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Final notunuzu giriniz= ");
+            int final = Convert.ToInt16(Console.ReadLine());
+
+            if (midterm<0 || midterm>100 || final<0 || final>100)
+            {
+                Console.WriteLine("Hatalı bir giriş yaptınız tekrar deneyiniz");
+            }
+            else
+            {
+                Console.WriteLine("Ortalama= "+((midterm*0.4)+(final*0.6)));
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exampleTax/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleTax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir ürünün kdv eklenmiş fiyatını bulalım
+
+            Console.WriteLine("Kdv eklenmesini istediğiniz ürünün fiyatını giriniz");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            if (price>=0)
+            {
+                Console.WriteLine(price * 1.18);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı bir giriş yaptınız");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
