7d8357be250152dfee641bb5ee5a37f21e5a1050
yasinekincii <ekinciyasinn21@gmail.com>
2022-12-25 14:48:47 +0300

    examples

---
+++ exampleFtoC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleFtoC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //F türünden girilen sıcaklığı C türüne çeviren uygulama
+
+            Console.WriteLine("Sıcaklığı giriniz= ");
+
+            double temperature = Convert.ToDouble(Console.ReadLine());
+            double result;
+
+            result = (temperature - 32) * 5 / 9;
+            Console.WriteLine("Girilen F derecesinin C karşılığı= " + result);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
