a655425e7aee02bac7591041c7f2b82b34c045b9
yasinekincii <ekinciyasinn21@gmail.com>
2022-12-26 19:20:36 +0300

    For

--- examplecb/Program.cs
+++ examplecb/Program.cs
@@ -8,43 +8,37 @@ using System.Threading.Tasks;
 namespace examplecb
 {
     internal class Program
-    {
-
-        private static int palindrom(string text,int length)
-        {
-            for (int i=length-1,j=0 ; i>=0; i--,j++ )
-            {
-                if (text[i] == text[j])
-                {
-                    length--;
-                }
-            }
-
-            length += 1;
-
-            if (length == 1)
-            {                
-                return length;
-            }
-            else
-            {
-                length = 0;
-                return length;
-
-            }          
-          
-        }
+    {  
 
         static void Main(string[] args)
         {
             //Tersi kendisiyle aynı olan metinlere palindrom denilir.Mesela “ama”, “madam” gibi.Verilen bir metin ve uzunlugunu kullanarak eger metin palindrom ise
             //1 degilse 0 döndüren özyinelemeli bir fonksiyon yazınız.Herhangi bir kütüphane fonksiyonu kullanamazsınız.
-            //Not: bas ̧ ve son karakterleri esit olan ve ortası palindrom olan metin palindromdur. int palindrom(String str, int n)
+            //Not: bas ̧ ve son karakterleri esit olan ve ortası palindrom olan metin palindromdur. int palindrom(String str, int n)          
 
-            Console.WriteLine(palindrom("madam",5));
+            Palindrom("ama");
 
-            Console.Read();
+            Console.ReadLine();          
 
         }
+
+        private static void Palindrom(string text)
+        {
+            int n = text.Length;
+            for(int i=text.Length-1,j=0; i>=0; i--,j++)
+            {
+                
+            }
+
+            if (n==0)
+            {
+                Console.Write("1");
+            }
+            else
+            {
+                Console.Write("0");
+            }
+        }
+
     }
 }
