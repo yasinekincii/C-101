90f28771201e62e866d5ec1bc2b4f5abde7e2dd2
yasinekincii <ekinciyasinn21@gmail.com>
2022-12-26 12:36:00 +0300

    Methods

---
+++ Methods1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1
+{
+    internal class Program
+    {
+        static void Main(string[] args)//main metodu
+        {
+            //Sonunda aç kapa bir parantez gördüğünde bu bir metottur
+            //metotlar sınıf içerisinde yer alırlar ve metodun dışarısında tanımlanırlar.
+            //metot içinde metot tanımlaması olmuyor.
+
+            //metodu class program sınıfının içerisinde tanımlıyoruz.
+
+
+            Yaz("Merhaba!");
+
+
+            var result = Sum(5,4);
+            Yaz(result.ToString());
+            Bosluk();
+            Yaz(Sum(10,10).ToString());
+
+
+            Yaz(GetAccountName(2));
+
+
+            var sonuc=IsValid("yasin.ekinci@hotmail.com");
+            Yaz(sonuc.ToString());
+
+            Beklet();//beklet metodunu burada çağırıyorum readLine()'ın yaptığı işi yapıcak.
+        }
+
+        static bool IsValid(string email)
+        {
+            bool isValid= default(bool);
+
+            if (email.Contains("@"))
+            {
+                isValid = true;
+            }
+
+            return isValid; 
+              
+        }
+
+        static string GetAccountName(int accountID)
+        {
+            string accountName = string.Empty;
+
+            switch (accountID)
+            {
+                case 1:
+                    accountName = "Yasin";
+                    break;
+                case 2:
+                    accountName = "Hayriye";
+                    break;
+                case 3:
+                    accountName = "Mustafa";
+                    break;
+                case 4:
+                    accountName = "Sena";
+                    break;
+
+                default:
+                    accountName = "Unknown";
+                    break;
+            }
+
+            return accountName;
+
+
+        }
+
+        static void Bosluklu(int sayi)
+        {
+            for (int i=0; i<sayi; i++)
+            {
+                Console.WriteLine();//Bu şekilde girdiğim sayi kadar boşluk bırakıcak
+            }
+        }
+
+        static void Bosluk()
+        {
+            Console.WriteLine(Environment.NewLine);//alt satıra indirme işlemi var
+        }
+
+        static int Sum(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+
+        static double Sum(double number1,double number2)//BU METOTLARIN AŞIRI YÜKLENMESİDİR.
+        {
+            return number1 + number2;
+        }
+
+        //bir tane bir metot adımız var.Bu metottan birden fazla varsa bu metot aşırı yüklenmiş demektir.
+
+
+        static void Yaz(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static void Beklet()
+        {
+            //beklet metodumuzun adı
+            //void bu metodun geri dönüş tipi
+            //bu metod geriye hiçbir şey döndürmeyecekse void olur
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result=Highest(1,2,3,4,5);//overloadlarda Metodun adını yazdıktan sonra parantez içerisinde aşağı yukarı yapabiliyorsun.Kaç tane parametreli olanı seçmek istersen
+            Console.WriteLine(result);
+
+
+            Console.WriteLine(Rastgele().ToString());
+
+
+            KullaniciEkle("yasin","ekinci");
+            KullaniciEkle("Elif","Ekinci",false);//opsiyonel olmasına rağmen bu şekilde false yazığ isactive değerini değiştirebiliyorum
+            Console.WriteLine("\n");
+
+            Team(
+                ad:"yasin",
+                soyad:"ekinci",
+                aktifMi:false,
+                oynar:false
+                
+                );
+
+            Console.ReadLine();
+        }
+
+        //İsimlendirilmiş parametreler(Named parameters)
+        static void Team(string ad,string soyad,bool aktifMi=true,bool oynar=true)
+        {
+            Console.WriteLine("Ad:"+ad+"\nSoyad:"+soyad+"\nAktif:"+aktifMi+"\nOynar:"+oynar);
+        }
+
+        //opsiyonel parametreler
+        static void KullaniciEkle(string name,string lastname,bool isActive=true)//eşittir true diyerek sen opsiyonelsin olmak zoruda değilsin demiş olduk
+        {
+            //opsiyonel parametreler parametrelerin en sağında yer alır
+            Console.WriteLine("Ad: "+name+"\nSoyad: "+lastname+"\nAktif: "+isActive);
+        }
+
+        static int Rastgele()
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(1000, int.MaxValue);
+        }
+
+        static int Highest(int number1,int number2)//2 parametreli gönderirsem buraya gitsin
+        {
+            if (number1>number2) {
+                return number1;            
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        static int Highest(int number1,int number2,int number3)//3 parametreli gönderirsem buraya gitsin
+        {
+            return Highest(Highest(number1,number2), number3);
+        }
+
+        //3'ten fazla ve belirsiz parametre kullanmak istersem kullanacağım metot
+        static int Highest(params int[] numbers)
+        { //int dizisi alıcaksın diyorum parametre olarak ama başına params koyuyorum
+
+            int sum = 0;
+            foreach (var sayi in numbers)
+            {
+                sum += sayi;
+            }
+
+            return sum;
+
+        }
+    }
+}
